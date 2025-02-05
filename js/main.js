d2cb1e10a54003eb18bfe5be486155e3713bf397
k-watson <watsonkingsley38@gmail.com>
2025-02-05 00:05:03 -0500

    added console logs

--- js/main.js
+++ js/main.js
@@ -1,2 +1,50 @@
 console.log("JS file connected")
 
+const bubble = document.querySelector('#bubble');
+
+function bubbleLogId() {
+    console.log(this.id);
+}
+
+bubble.addEventListener('click', bubbleLogId);
+
+const burple1 = document.querySelector('#burple1');
+
+function burple1LogId() {
+    console.log(this.id);
+}
+
+burple1.addEventListener('click', burple1LogId);
+
+const type = document.querySelector('#type');
+
+function typeLogId() {
+    console.log(this.id);
+}
+
+type.addEventListener('click', typeLogId);
+
+const burple = document.querySelector('#burple');
+
+function burpleLogId() {
+    console.log(this.id);
+}
+
+burple.addEventListener('click', burpleLogId);
+
+const burple_text = document.querySelector('#burple_text');
+
+function burple_textLogId() {
+    console.log(this.id);
+}
+
+burple_text.addEventListener('click', burple_textLogId);
+
+const bubble1 = document.querySelector('#bubble1');
+
+function bubble1LogId() {
+    console.log(this.id);
+}
+
+bubble1.addEventListener('click', bubble1LogId);
+
